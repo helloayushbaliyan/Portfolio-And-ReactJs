c504163d44860415e4284dcd6f6fba83f04f0abb
Ayush Baliyan <helloayushbaliyan@gmail.com>
2025-03-03 13:18:40 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <div className=" bg-slate-950 h-screen ">
+      <h1 className='text-4xl text-white text-center'>here we start making our react first portfolio</h1>
+    </div>
+    </>
+  )
+}
+
+export default App
