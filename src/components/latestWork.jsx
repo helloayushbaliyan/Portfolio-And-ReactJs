0040401c284cf54b8d1ba952e260782391eb10fd
Ayush Baliyan <helloayushbaliyan@gmail.com>
2025-05-12 22:32:12 +0530

    add laetst work button

--- src/components/latestWork.jsx
+++ src/components/latestWork.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import TextCard from "./textCard";
+import WhiteBtn from "./whiteBtn";
 
 export default function LatestWork(props) {
   const textCard = [
@@ -9,7 +10,7 @@ export default function LatestWork(props) {
       subHeading: "",
       description: "",
       textSize:
-        "text-[2.2rem]  z-[100] lg:text-[{3rem}] xl:text-[3.5rem]    2xl:text-[5rem]  leading-[3.2rem] 2xl:leading-[6rem] md:leading-[4rem]",
+        "text-[2.2rem]  z-[100] lg:text-[{3rem}] xl:text-[3.5rem] text-white   2xl:text-[5rem]  leading-[3.2rem] 2xl:leading-[6rem] md:leading-[4rem]",
       profileSrc: "",
     },
   ];
@@ -76,42 +77,43 @@ export default function LatestWork(props) {
   ];
   return (
     <>
-      <div className=" font-[inter] text-center my-[6rem] px-4 sm:px-12 md:px-[3rem]  xl:px-[7.5rem] 2xl:px-[10rem]  ">
-        <div className="">
-          <div className="flex justify-center   ">
-            <TextCard key={work.id} data={textCard[0]} />
-          </div>
-          <div className="hidden md:flex justify-center  space-x-12 mx-auto mt-8">
-            {work.map((work) => (
-              <p is key={work.sucess} className=" relative text-[#91919b]">
-                {work.title}
-                <span className=" absolute top-[-16px] right-[-16px]">
-                  {work.sucess}
-                </span>
-              </p>
-            ))}
-          </div>
-          <select
-            className="w-full bg-white outline-0 rounded-xl text-xl py-4 px-2 md:hidden"
-            name=""
-            id=""
-          >
-            {work.map((work) => (
-              <option
-                is
-                key={work.sucess}
-                className="text-xl py-4 px-2  text-[#91919b]"
+      <div className="bg-[#28293e] mt-[6rem]">
+        <div className="  px-4 sm:px-12 md:px-[3rem] xl:px-[7.5rem] 2xl:px-[10rem]  py-[5rem] space-y-[5rem]">
+          <div className=" font-[inter] text-center  ">
+            <div className="">
+              <div className="flex justify-center   ">
+                <TextCard key={work.id} data={textCard[0]} />
+              </div>
+              <div className="hidden md:flex justify-center  space-x-12 mx-auto mt-8">
+                {work.map((work) => (
+                  <p is key={work.sucess} className=" relative text-[#91919b]">
+                    {work.title}
+                    <span className=" absolute top-[-16px] right-[-16px]">
+                      {work.sucess}
+                    </span>
+                  </p>
+                ))}
+              </div>
+              <select
+                className="w-full bg-white outline-0 rounded-xl text-xl py-4 px-2 md:hidden"
+                name=""
+                id=""
               >
-                {work.title}
-              </option>
-            ))}
-          </select>
-        </div>
-      </div>
-      <div className="bg-[#28293e]">
-        <div className="  px-4 sm:px-12 md:px-[3rem] xl:px-[7.5rem] 2xl:px-[10rem]  py-[5rem]">
+                {work.map((work) => (
+                  <option
+                    is
+                    key={work.sucess}
+                    className="text-xl py-4 px-2  text-[#91919b]"
+                  >
+                    {work.title}
+                  </option>
+                ))}
+              </select>
+            </div>
+          </div>
+
           <div className=" relative ">
-            <div className="  top-[-12rem]  grid sm:grid-cols-2 lg:grid-cols-3 w-full 2xl:gap-[2rem] gap-6">
+            <div className="   grid sm:grid-cols-2 lg:grid-cols-3 w-full 2xl:gap-[2rem] gap-6">
               {lists.map((list) => (
                 <div className="w-[100%] relative ">
                   <img
@@ -129,6 +131,9 @@ export default function LatestWork(props) {
                   </div>
                 </div>
               ))}
+            </div>
+            <div className="flex justify-center">
+              <WhiteBtn btntxt="Explore More" />
             </div>
           </div>
         </div>
