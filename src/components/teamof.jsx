cbefd4d3fab58640cf247e623469a7b6c2f64d96
Ayush Baliyan <helloayushbaliyan@gmail.com>
2025-05-22 11:58:01 +0530

    add horizontal scroll in image gallary

--- src/components/teamof.jsx
+++ src/components/teamof.jsx
@@ -35,11 +35,47 @@ export default function Teamof(props) {
       field: "Product Designer",
     },
     {
+      src: "/images/team_1.png",
+      title_name: "Azah Anyeni",
+      field: "Designer",
+    },
+    {
+      src: "/images/team_2.png",
+      title_name: "Roelof Bekkenenks",
+      field: "React Developer",
+    },
+    {
+      src: "/images/team_3.png",
+      title_name: "Leonardo Oliveira",
+      field: "Illustrator",
+    },
+    {
       src: "/images/team_4.png",
       title_name: "Izabella Tabakova",
       field: "Product Designer",
     },
+
   ];
+
+  const previousBtn = () => {
+    const scroll = document.querySelector(".wheel");
+    scroll.scrollLeft -= 1105;
+    scroll.style.scrollBehavior = "smooth";
+    const addcolor = document.querySelector(".second");
+    addcolor.classList.remove("bg-[#ef6d58]");
+    const removecolor = document.querySelector(".first");
+    removecolor.classList.add("bg-[#ef6d58]");
+  };
+
+  const nextBtn = () => {
+    const scroll = document.querySelector(".wheel");
+    scroll.style.scrollBehavior = "smooth";
+    scroll.scrollLeft += 1105;
+    const color = document.querySelector(".second");
+    color.classList.add("bg-[#ef6d58]");
+    const removecolor = document.querySelector(".first");
+    removecolor.classList.remove("bg-[#ef6d58]");
+  };
 
   return (
     <>
@@ -49,15 +85,22 @@ export default function Teamof(props) {
             <TextCard data={textCard[0]} />
           </div>
           <div className="hidden lg:flex items-center mt-[5rem] gap-3">
-            <button className="border-2 border-slate-500  w-12 h-12 flex justify-center hover:scale-110  transition duration-200 rounded-full">
+            <button
+              onClick={previousBtn}
+              className="border-2 border-slate-500  w-12 h-12 flex justify-center hover:scale-110  transition duration-200 rounded-full"
+            >
               <img className="w-6 " src="/images/arrow.svg" alt="" />
             </button>
-            <button className="border-2 border-slate-500  w-12 h-12 flex justify-center hover:scale-110  transition duration-200 rounded-full">
+
+            <button
+              onClick={nextBtn}
+              className="border-2 border-slate-500  w-12 h-12 flex justify-center hover:scale-110  transition duration-200 rounded-full"
+            >
               <img className="w-6 rotate-180" src="/images/arrow.svg" alt="" />
             </button>
           </div>
         </div>
-        <div className="flex gap-8 overflow-x-scroll lg:overflow-x-hidden py-6 mt-3">
+        <div className="wheel flex gap-8 overflow-x-scroll lg:overflow-x-hidden py-6 mt-3">
           {teamList.map((meamber) => (
             <div className="w-full space-y-5 ">
               <div className="border-2 border-[#3a3c56] rounded-xl w-[15.3rem] flex justify-center pt-6 px-1 max-h-[15rem] h-full">
@@ -71,10 +114,9 @@ export default function Teamof(props) {
           ))}
         </div>
 
-        <div className="flex justify-center space-x-4 mt-5">
-          <div className="border-2 border-[#3a3c56] rounded-full p-1.5 bg-[#ef6d58]"></div>
-          <div className="border-2 border-[#3a3c56] rounded-full p-1.5 "></div>
-          <div className="border-2 border-[#3a3c56] rounded-full p-1.5 "></div>
+        <div className=" hidden lg:flex justify-center space-x-4 mt-5">
+          <div className="first border-2 border-[#3a3c56] rounded-full p-1.5 bg-[#ef6d58]"></div>
+          <div className="second border-2 border-[#171e8c] rounded-full p-1.5 "></div>
         </div>
       </div>
     </>
